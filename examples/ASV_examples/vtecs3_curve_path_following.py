58f48d5b4b6615d817f2b3ba264c5c1c06c66361
alexglzg <alexglzg@mit.edu>
2023-02-07 12:21:08 +0100

    curved path following examples

--- examples/ASV_examples/vtecs3_curve_path_following.py
+++ examples/ASV_examples/vtecs3_curve_path_following.py
@@ -31,6 +31,7 @@ from casadi import *
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import minimize
 
 # -------------------------------
 # Problem parameters
@@ -53,44 +54,44 @@ Iz = 4.1
 B = 0.41
 c = 1.0
 
-nx    = 14                   # the system is composed of 14 states
-nu    = 2                   # the system has 2 input
+nx    = 9                   # the system is composed of 7 states
+nu    = 2                   # the system has 2 inputs
 Tf    = 1                   # control horizon [s]
 Nhor  = 20                  # number of control intervals
 dt    = Tf/Nhor             # sample time
 
-starting_angle = 0.00
-x_1 = 4.0
-y_1 = -5.0
-x2 = 4.0
-y2 = 25.0
-a_k = np.math.atan2(y2-y_1, x2-x_1)
-ned_x = 0.1
-ned_y = 0.1
-y_e = -(ned_x-x_1)*np.sin(a_k)+(ned_y-y_1)*np.cos(a_k)
-current_X = vertcat(starting_angle, np.sin(starting_angle), np.cos(starting_angle), 0.001, 0.00, 0.00, y_e, x_1, y_1, a_k, ned_x, ned_y, 0.00, 0.00)  # initial state
+starting_angle = 0.0
+ned_x = 0.0
+ned_y = 0.0
+u_ref = 1.0
 
-u_ref = 1.4
-ak_ref = a_k
-sinpsi_ref = np.sin(ak_ref)
-cospsi_ref = np.cos(ak_ref)
-ye_ref = 0.0
-final_X   = vertcat(0, sinpsi_ref, cospsi_ref, u_ref, 0, 0, ye_ref, 0, 0, 0, 0, 0, 0, 0)    # desired terminal state
+x_multiplier = 0.5
+y_amplitude = 2.0
+y_freq = 3*np.pi/40
 
-Nsim  = int(20 * Nhor / Tf)#200                 # how much samples to simulate
-add_noise = False #True            # enable/disable the measurement noise addition in simulation
-add_disturbance = False #True      # enable/disable the disturbance addition in simulation
+def desired_x(s_var):
+    return x_multiplier*s_var
+
+def desired_y(s_var):
+    return y_amplitude*np.sin(s_var*y_freq)
+
+def path_w_args(s_var, xpos, ypos):
+    return ((xpos-desired_x(s_var))**2 + (ypos-desired_y(s_var))**2)
+s_0 = minimize(path_w_args, 0, method='nelder-mead', args=(ned_x, ned_y), options={'xatol': 1e-8, 'disp': True})
+s_0 = s_0.x
+
+current_X = vertcat(ned_x,ned_y,starting_angle,0,0,0,0,0,s_0)  # initial state
+
+Nsim  = int(40 * Nhor / Tf)                 # how much samples to simulate
 
 # -------------------------------
 # Logging variables
 # -------------------------------
-'''pos_history     = np.zeros(Nsim+1)
-theta_history   = np.zeros(Nsim+1)
-F_history       = np.zeros(Nsim)'''
-ye_history     = np.zeros(Nsim+1)
-theta_history   = np.zeros(Nsim+1)
 x_history     = np.zeros(Nsim+1)
 y_history   = np.zeros(Nsim+1)
+yaw_history     = np.zeros(Nsim+1)
+xd_history     = np.zeros(Nsim+1)
+yd_history   = np.zeros(Nsim+1)
 u_history     = np.zeros(Nsim+1)
 r_history   = np.zeros(Nsim+1)
 Tport_history       = np.zeros(Nsim+1)
@@ -100,22 +101,16 @@ Tstbd_history       = np.zeros(Nsim+1)
 # Set OCP
 # -------------------------------
 ocp = Ocp(T=Tf)
-
 # Define states
+nedx = ocp.state()
+nedy = ocp.state()
 psi = ocp.state()
-sinpsi = ocp.state() 
-cospsi = ocp.state() 
 u = ocp.state()
 v = ocp.state()
 r = ocp.state()
-ye = ocp.state()
-x1 = ocp.state()
-y1 = ocp.state()
-ak = ocp.state()
-nedx = ocp.state()
-nedy = ocp.state()
 Tport = ocp.state()
 Tstbd = ocp.state()
+s = ocp.state()
 ocp.set_initial(u,0.001)
 
 # Defince controls
@@ -126,6 +121,12 @@ UTstbddot = ocp.control()
 X_0 = ocp.parameter(nx)
 
 # Specify ODE
+x_d = x_multiplier*s
+y_d = y_amplitude*np.sin(s*y_freq)
+xdot_d = x_multiplier
+ydot_d = y_amplitude*y_freq*cos((y_freq) * s)
+gamma_p = atan2(ydot_d, xdot_d)
+ye = -(nedx-x_d)*sin(gamma_p)+(nedy-y_d)*cos(gamma_p)
 Xu = if_else(u > 1.25, 64.55, -25)
 Xuu = if_else(u > 1.25, -70.92, 0)
 Yv = 0.5*(-40*1000*fabs(v))*(1.1+0.0045*(1.01/0.09)-0.1*(0.27/0.09)+0.016*((0.27/0.09)*(0.27/0.09)))
@@ -134,37 +135,39 @@ Tu = Tport + c * Tstbd
 Tr = (Tport - c * Tstbd) * B / 2
 beta = atan2(v,u+.001)
 chi = psi + beta
+ocp.set_der(nedx, (u*cos(psi) - v*sin(psi)))
+ocp.set_der(nedy, (u*sin(psi) + v*cos(psi)))
 ocp.set_der(psi, r)
-ocp.set_der(sinpsi, (cos(chi)*r))
-ocp.set_der(cospsi, (-sin(chi)*r))
 ocp.set_der(u, ((Tu - (-m + 2 * Y_v_dot)*v - (Y_r_dot + N_v_dot)*r*r - (-Xu*u - Xuu*fabs(u)*u)) / (m - X_u_dot)))
 ocp.set_der(v, ((-(m - X_u_dot)*u*r - (- Yv - Yvv*fabs(v) - Yvr*fabs(r))*v) / (m - Y_v_dot)))
 ocp.set_der(r, ((Tr - (-2*Y_v_dot*u*v - (Y_r_dot + N_v_dot)*r*u + X_u_dot*u*r) - (-Nr*r - Nrv*fabs(v)*r - Nrr*fabs(r)*r)) / (Iz - N_r_dot)))
-ocp.set_der(ye, (-(u*cos(psi) - v*sin(psi))*sin(ak) + (u*sin(psi) + v*cos(psi))*cos(ak)))
-ocp.set_der(x1, 0)
-ocp.set_der(y1, 0)
-ocp.set_der(ak, 0)
-ocp.set_der(nedx, (u*cos(psi) - v*sin(psi)))
-ocp.set_der(nedy, (u*sin(psi) + v*cos(psi)))
 ocp.set_der(Tport, UTportdot)
 ocp.set_der(Tstbd, (UTstbddot/c))
+ocp.set_der(s, u)
+
+Qye = 100.0
+Qr = 0.025
+Qpsi = 50.0
+Qu = 10.0
+R = 0.005
+#QNye = 50.1
+#QNr = 0.05
 
 # Lagrange objective
-ocp.add_objective(ocp.integral(0.4*(ye-ye_ref)**2 + 0.15*(sinpsi-sinpsi_ref)**2 + 0.15*(cospsi-cospsi_ref)**2 + 40*(u-u_ref)**2 + 0.00005*Tstbd**2 + 0.00005*Tport**2))
-ocp.add_objective(ocp.at_tf(0.5*(ye-ye_ref)**2 + 0.25*(sinpsi-sinpsi_ref)**2 + 0.25*(cospsi-cospsi_ref)**2 + 0*(u-u_ref)**2 + 0.00025*Tstbd**2 + 0.00025*Tport**2))
+ocp.add_objective(ocp.integral(Qye*(ye**2) + Qpsi*(psi-gamma_p)**2 + Qu*(u-u_ref)**2))
+ocp.add_objective(ocp.at_tf(Qye*(ye**2) + Qpsi*(psi-gamma_p)**2 + Qu*(u-u_ref)**2))
 
 # Path constraints
 ocp.subject_to( (-2.0 <= u) <= 2.0 )
-ocp.subject_to( (-10.0 <= r) <= 10.0 )
 ocp.subject_to( (-30.0 <= Tport) <= 35.0 )
 ocp.subject_to( (-30.0 <= Tstbd) <= 35.0 )
 ocp.subject_to( (-30.0 <= UTportdot) <= 30.0 )
 ocp.subject_to( (-30.0 <= UTstbddot) <= 30.0 )
+ocp.subject_to( s >= 0)
 
 # Initial constraints
-X = vertcat(psi, sinpsi, cospsi, u, v, r, ye, x1, y1, ak, nedx, nedy, Tport, Tstbd)
+X = vertcat(nedx,nedy,psi,u,v,r,Tport,Tstbd,s)
 ocp.subject_to(ocp.at_t0(X)==X_0)
-#ocp.subject_to(ocp.at_tf(X)==final_X)
 
 # Pick a solution method
 options = {"ipopt": {"print_level": 0}}
@@ -184,19 +187,18 @@ ocp.set_value(X_0, current_X)
 sol = ocp.solve()
 
 # Get discretisd dynamics as CasADi function
-#Sim_pendulum_dyn = ocp._method.discrete_system(ocp)
 Sim_asv_dyn = ocp._method.discrete_system(ocp)
 
 # Log data for post-processing
-#ye_history[0]   = current_X[6]
-ye_history[0]   = current_X[4]
-theta_history[0] = current_X[0]
-x_history[0]   = current_X[10]
-y_history[0] = current_X[11]
+x_history[0]   = current_X[0]
+y_history[0] = current_X[1]
+yaw_history[0]   = current_X[2]
+xd_history[0]   = desired_x(s_0)
+yd_history[0] = desired_y(s_0)
 u_history[0]   = current_X[3]
 r_history[0] = current_X[5]
-Tport_history[0] = current_X[12]
-Tstbd_history[0] = current_X[13]
+Tport_history[0] = current_X[6]
+Tstbd_history[0] = current_X[7]
 
 # -------------------------------
 # Simulate the MPC solving the OCP (with the updated state) several times
@@ -209,43 +211,37 @@ for i in range(Nsim):
     _, Tssol = sol.sample(UTstbddot, grid='control')
     # Simulate dynamics (applying the first control input) and update the current state
     current_X = Sim_asv_dyn(x0=current_X, u=vertcat(Tpsol[0],Tssol[0]), T=dt)["xf"]
+    # Compute new starting s0
+    s_0 = minimize(path_w_args, 0, method='nelder-mead', args=(current_X[0], current_X[1]), options={'xatol': 1e-8, 'disp': True})
+    s_0 = s_0.x
     # Set the parameter X0 to the new current_X
-    ocp.set_value(X_0, current_X[:14])
+    ocp.set_value(X_0, vertcat(current_X[:8],s_0))
     # Solve the optimization problem
     sol = ocp.solve()
     ocp._method.opti.set_initial(ocp._method.opti.x, ocp._method.opti.value(ocp._method.opti.x))
 
     # Log data for post-processing
-    ye_history[i+1]   = current_X[6].full()
-    theta_history[i+1] = current_X[0].full()
-    x_history[i+1]   = current_X[10].full()
-    y_history[i+1] = current_X[11].full()
+    x_history[i+1]   = current_X[0].full()
+    y_history[i+1] = current_X[1].full()
+    yaw_history[i+1]   = current_X[2].full()
     u_history[i+1]   = current_X[3].full()
     r_history[i+1] = current_X[5].full()
-    Tport_history[i+1] = current_X[12].full()
-    Tstbd_history[i+1] = current_X[13].full()
+    Tport_history[i+1] = current_X[6].full()
+    Tstbd_history[i+1] = current_X[7].full()
+    xd_history[i+1]   = desired_x(s_0)
+    yd_history[i+1] = desired_y(s_0)
 
 # -------------------------------
 # Plot the results
 # -------------------------------
 time_sim = np.linspace(0, dt*Nsim, Nsim+1)
-
-fig, ax1 = plt.subplots()
-ax1.plot(time_sim, ye_history, 'r-')
-ax1.set_xlabel('Time [s]')
-ax1.set_ylabel('Cross-track error [m]', color='r')
-ax1.tick_params('y', colors='r')
-ax2 = ax1.twinx()
-ax2.plot(time_sim, theta_history, 'b-')
-ax2.set_ylabel('Heading angle [rad]', color='b')
-ax2.tick_params('y', colors='b')
-fig.tight_layout()
+time_sim2 = np.linspace(0, dt*Nsim, Nsim)
 
 fig2, ax3 = plt.subplots()
-ax3.plot(y_history, x_history, 'r-')
+ax3.plot(yd_history, xd_history, 'r-')
+ax3.plot(y_history, x_history, 'b--')
 ax3.set_xlabel('Y [m]')
-ax3.set_ylabel('X [m]', color='r')
-ax3.tick_params('y', colors='r')
+ax3.set_ylabel('X [m]')
 
 fig3, ax4 = plt.subplots()
 ax4.plot(time_sim, Tport_history, 'r-')
@@ -259,34 +255,20 @@ ax5.tick_params('y', colors='b')
 fig3.tight_layout()
 
 fig4, ax6 = plt.subplots()
-ax6.plot(time_sim, u_history, 'r-')
+ax6.plot(time_sim, yaw_history, 'b-')
 ax6.set_xlabel('Time [s]')
-ax6.set_ylabel('u [m/s]', color='r')
-ax6.tick_params('y', colors='r')
-ax7 = ax6.twinx()
-ax7.plot(time_sim, r_history, 'b-')
-ax7.set_ylabel('r [rad/s]', color='b')
-ax7.tick_params('y', colors='b')
+ax6.set_ylabel('yaw [rad]')
 fig4.tight_layout()
 
+fig5, ax7 = plt.subplots()
+ax7.plot(time_sim, u_history, 'r-')
+ax7.set_xlabel('Time [s]')
+ax7.set_ylabel('u [m/s]', color='r')
+ax7.tick_params('y', colors='r')
+ax8 = ax7.twinx()
+ax8.plot(time_sim, r_history, 'b-')
+ax8.set_ylabel('r [rad/s]', color='b')
+ax8.tick_params('y', colors='b')
+fig5.tight_layout()
 
 plt.show()
-
-# -------------------------------
-# Animate results
-# -------------------------------
-'''if plt.isinteractive():
-  fig2, ax3 = plt.subplots(1, 1)
-  plt.ion()
-  ax3.set_xlabel("X [m]")
-  ax3.set_ylabel("Y [m]")
-  for k in range(Nsim+1):
-      cart_pos_k      = ye_history[k]
-      theta_k         = theta_history[k]
-      pendulum_pos_k  = vertcat(horzcat(cart_pos_k,0), vertcat(cart_pos_k-L*sin(theta_k),L*cos(theta_k)).T)
-      color_k     = 3*[0.95*(1-float(k)/Nsim)]
-      ax3.plot(pendulum_pos_k[0,0], pendulum_pos_k[0,1], "s", markersize = 15, color = color_k)
-      ax3.plot(pendulum_pos_k[:,0], pendulum_pos_k[:,1], "-", linewidth = 1.5, color = color_k)
-      ax3.plot(pendulum_pos_k[1,0], pendulum_pos_k[1,1], "o", markersize = 10, color = color_k)
-      plt.pause(dt)
-plt.show(block=True)'''
